b0b50039f6d8720d6fec944890aa2dfb86bade8a
Alberto Ocroche <albrom@gmail.com>
2021-06-04 10:53:41 -0300

    Testes com Models de Processos

--- apps/produtos/views.py
+++ apps/produtos/views.py
@@ -1,6 +1,7 @@
-import django_filters
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
+from .filters import ProdutoFilter
 from .models.produtos import Produto
 
 
@@ -29,16 +30,3 @@ class ProdutoEdit(UpdateView):
     model = Produto
     fields = ['descricao', 'unidade', 'sigla', 'subgrupo', 'especifica',
               'numprotheus', 'tabela_eventos']
-
-
-class ProdutoFilter(django_filters.FilterSet):
-    descricao = django_filters.CharFilter(lookup_expr='icontains')
-
-    class Meta:
-        model = Produto
-        fields = ['sigla', 'descricao', 'subgrupo', 'tabela_eventos']
-
-    @property
-    def qs(self):
-        parent = super().qs
-        return parent.filter(ativo=True)
--- sisgecon/settings.py
+++ sisgecon/settings.py
@@ -16,8 +16,8 @@ from pathlib import Path
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,11 +78,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'sisgecon.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [BASE_DIR / 'templates']
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [BASE_DIR / 'templates']
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
